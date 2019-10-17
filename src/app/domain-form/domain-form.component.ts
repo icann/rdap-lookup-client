9d02b3574fe4f8c677d10a7ff86fe9ca038f35d9
Santosh Dumbre <santosh.dumbre@zensar.com>
2019-10-17 19:52:34 +0530

    domain form component related cleanup

--- src/app/domain-form/domain-form.component.ts
+++ src/app/domain-form/domain-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit} from '@angular/core';
 import { AcronymService } from '../services/acronym.service';
 import { LookupService, MessageTypes } from '../services/lookup.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'rwc-domain-form',
@@ -16,7 +15,7 @@ export class DomainFormComponent implements OnInit {
   domain: string;
   public messageTypes = MessageTypes;
 
-  constructor (private router: Router, public acronymService: AcronymService, public lookupService: LookupService) {}
+  constructor (public acronymService: AcronymService, public lookupService: LookupService) {}
 
   ngOnInit () {
     if (this.lookupService.getDomain()) {
@@ -32,18 +31,6 @@ export class DomainFormComponent implements OnInit {
 
   submitRequest (): void {
     this.lookupService.sendRequest();
-  }
-
-  goToHomeAnchorDomainTou (): void {
-    // Prevent angular router not refreshing click for same route anchor
-    const url = this.router.url.split('#');
-
-    if (url[0] === '/') {
-      window.location.hash = '';
-      window.location.hash = '#domaintou';
-    } else {
-      this.router.navigate(['/'], { fragment: 'domaintou' });
-    }
   }
 
 }
